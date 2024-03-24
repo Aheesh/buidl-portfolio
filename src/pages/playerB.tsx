0a1550255fca085ec2d36b8a58649ca96ac8e69b
aheesh <aheesh@gmail.com>
2024-03-23 20:21:05 -0700

    installed onchain kit, FrameMetadata on playerB page.

--- src/pages/playerB.tsx
+++ src/pages/playerB.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { ApproveERC20 } from "@/components/approveERC20";
 import { BuyPlayerBWinToken } from "@/components/buyPlayerBWinToken";
 
+import { FrameMetadata } from "@coinbase/onchainkit";
+
 export default function SwapPageB() {
   return (
     <main>
@@ -14,6 +16,31 @@ export default function SwapPageB() {
         width="2000"
         className="fixed z-[-1] top-0 left-0 object-cover md:h-96 min-h-48 w-full"
       />
+      <FrameMetadata
+        buttons={[
+          {
+            label: "Tell me the story",
+          },
+          {
+            action: "link",
+            label: "Link to Google",
+            target: "https://www.google.com",
+          },
+          {
+            action: "post_redirect",
+            label: "Redirect to cute pictures",
+          },
+        ]}
+        image={{
+          src: "https://zizzamia.xyz/park-3.png",
+          aspectRatio: "1:1",
+        }}
+        input={{
+          text: "Tell me a boat story",
+        }}
+        postUrl="https://zizzamia.xyz/api/frame"
+      />
+
       <div className="fixed top-3 right-3 z-10">
         <ConnectKitButton />
       </div>
